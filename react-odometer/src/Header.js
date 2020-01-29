3bafa8c0a8dbf016895d836e430bf6defa7ce450
nastinsk <nastinsk@gmail.com>
2020-01-29 00:33:30 -0800

    initial header and footer placed on the page

---
+++ react-odometer/src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Odometer with React</h1>
+      <p>some lorem ipsum</p>
+      </>
+    )
+  }
+}
+
+export default Header;
---
+++ react-odometer/src/Odometer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Odometer extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+      <h2>This is Odometer</h2>
+      <p>Some numbers</p>
+      </>
+    )
+  }
+}
+
+export default Odometer;
